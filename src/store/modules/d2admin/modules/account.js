3ccb3d94cc1588ef4d90d0a447a0bca0a82dc611
zhaohaochen <aassaadd@qq.com>
2020-09-24 21:36:25 +0800

    接入登陆框架

--- src/store/modules/d2admin/modules/account.js
+++ src/store/modules/d2admin/modules/account.js
@@ -15,23 +15,43 @@ export default {
      */
     async login ({ dispatch }, {
       username = '',
-      password = ''
+      password = '',
+      loginFunc = null
     } = {}) {
       // const res = await api.SYS_USER_LOGIN({ username, password })
-      const res = {
-        uuid: '1',
-        token: '2',
-        name: '3'
+      let res = {
+        uuid: '',
+        token: '',
+        name: ''
+      }
+      if (loginFunc) {
+        try {
+          res = await loginFunc({
+            body: {
+              username,
+              password
+            }
+          })
+          util.cookies.set('uuid', res.id)
+          util.cookies.set('token', res.token)
+          await dispatch('d2admin/user/set', {
+            name: res.name,
+            hasBuild: false
+          }, { root: true })
+        } catch (e) {
+          console.log(e)
+          await dispatch('logout')
+        }
       }
       // 设置 cookie 一定要存 uuid 和 token 两个 cookie
       // 整个系统依赖这两个数据进行校验和存储
       // uuid 是用户身份唯一标识 用户注册的时候确定 并且不可改变 不可重复
       // token 代表用户当前登录状态 建议在网络请求中携带 token
       // 如有必要 token 需要定时更新，默认保存一天
-      util.cookies.set('uuid', res.uuid)
-      util.cookies.set('token', res.token)
+      // util.cookies.set('uuid', res.uuid)
+      // util.cookies.set('token', res.token)
       // 设置 vuex 用户信息
-      await dispatch('d2admin/user/set', { name: res.name }, { root: true })
+      // await dispatch('d2admin/user/set', { name: res.name }, { root: true })
       // 用户登录后从持久化数据加载一系列的设置
       await dispatch('load')
     },
